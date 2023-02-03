962dc6d7f124c21c10e485eb7c9f24430d75309b
Georgiy <lvl_slep@mail.ru>
2023-02-03 18:43:27 +0300

    add filename to .gitignore

--- command/views.py
+++
@@ -1,7 +0,0 @@
-class CommandContextMixin:
-    title = None
-
-    def get_context_data(self, **kwargs):
-        context = super(CommandContextMixin, self).get_context_data(**kwargs)
-        context['title'] = self.title
-        return context
--- store/pipeline.py
+++
@@ -1,5 +0,0 @@
-def social(response, *args, **kwargs):
-    user = kwargs['user']
-    if user.email:
-        user.is_verified_email = True
-        user.save()
